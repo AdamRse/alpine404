b25450d40c115b38410e25d9019a7f745f7e3b96
AdamRse <bakaarion3@gmail.com>
2024-06-10 12:27:36 +0200

    ajoutt de la fonction de routage, faire un npm install pour l'installer, elle est dans le package.json normallement

---
+++ src/components/Configurateur.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Configurateur() {
+  return (
+    <>
+      <div>page2</div>
+      <a href='/'>Aller page 1</a>
+    </>
+  )
+}
+
+export default Configurateur
