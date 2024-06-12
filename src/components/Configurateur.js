499f507d04be7637f838080e5b24e12148e88df4
AdamRse <bakaarion3@gmail.com>
2024-06-12 12:09:01 +0200

    fix du sélécteur, il faudra juste changer la police (dans tous les sens du terme)

--- src/components/Configurateur.js
+++ src/components/Configurateur.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -11,26 +10,29 @@ function Configurateur() {
 
   function selecteurMotion(){
     let defaultLeftMargin = 10;
-    if(selectHover===0) defaultLeftMargin = 0
-    else if(selectHover===1) defaultLeftMargin = defaultLeftMargin*2;
+    if(selectHover===0) defaultLeftMargin = -defaultLeftMargin*2
+    else if(selectHover===1) defaultLeftMargin = 0;
+    else defaultLeftMargin = -defaultLeftMargin
+    console.log(selectHover, defaultLeftMargin);
     return {left: defaultLeftMargin+"%"}
   }
 
   return (
     <>
       <section id='choix'>
-        <div className='conteneur'>
+        <div className='conteneur' style={selecteurMotion()}>
           <img src={bgPure} />
           <img src={bgLegende} style={{transform: "scaleX(-1)"}} />
         </div>
         <div className='selecteur'>
+            Select a car
             <div className='conteneur-images'>
               <div className='selcteur-card'>
-                <img src={bgPure} onMouseEnter={setSelectHover(1)} onMouseLeave={selectHover(false)} />
+                <img src={bgPure} onMouseEnter={() => { setSelectHover(1) }} onMouseLeave={() => { setSelectHover(false)}} />
               </div>
               
               <div className='selcteur-card'>
-                <img src={bgLegende} style={{transform: "scaleX(-1)"}} onMouseEnter={setSelectHover(0)} onMouseLeave={selectHover(false)} />
+                <img src={bgLegende} style={{transform: "scaleX(-1)"}} onMouseEnter={() => { setSelectHover(0)}} onMouseLeave={() => { setSelectHover(false)}} />
               </div>
             </div>
           </div>
