66cc6c0b3dbe5d6b1eccee0ba60db5b0e9dd8a9b
AdamRse <bakaarion3@gmail.com>
2024-06-13 12:28:55 +0200

    Fix le système de react-dom

---
+++ src/components/Configurateur/Peinture.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSelection } from '../../store/slice';
+
+import bgPure from "../../images/configurateur/modele/selection/pure.png";
+import bgLegende from "../../images/configurateur/modele/selection/legende.png";
+
+function SelecteurAlpine() {
+
+    const dispatch = useDispatch();
+    const [selectHover, setSelectHover] = useState(false);
+    const selection = useSelector(state => state.alpine.selection);
+
+    // dispatch(
+    //     manageActionFree(false)
+    // );
+
+    function selecteurMotion() {
+
+    }
+    function chooseAlpine(model){
+        dispatch(
+            changeSelection({section: "model", value:model})
+        );
+    }
+
+    return (
+        <section id="peinture">
+            <img src='../../images/configurateur/modele/selection/pure.png' />
+        </section>
+    )
+}
+
+export default SelecteurAlpine
